c6ddd2670c919d0df5341eb6ec6844577e2a7606
WillisKE <130226179+WillisKE@users.noreply.github.com>
2024-01-13 02:57:50 +0300

    ♥️

---
+++ plugins/dl-spotify.js
@@ -0,0 +1,42 @@
+import fetch from 'node-fetch';
+import displayLoadingScreen from '../lib/loading.js';
+let handler = async (m, { conn, text }) => {
+    if (!text) {
+        console.log('No song name provided.');
+        throw `*Please enter a song name*`;
+    }
+  m.react('🎶')
+  await displayLoadingScreen(conn, m.chat);
+  let pp = 'https://i.imgur.com/7jTrcTu.jpeg'
+    const query = encodeURIComponent(text);
+    let res = `https://guruapi.tech/api/spotifydl?url=${query}`
+   // let spotify = await (await fetch(res)).buffer()
+    let doc = {
+        audio: {
+          url: res
+        },
+        mimetype: 'audio/mpeg',
+        ptt: true,
+        waveform:  [100, 0, 100, 0, 100, 0, 100],
+        fileName: "Guru.mp3",
+    
+        contextInfo: {
+          mentionedJid: [m.sender],
+          externalAdReply: {
+            title: "↺ |◁ ❚❚ ▷| ♡",
+            body: `Now playing: ${text}`,
+            thumbnailUrl: pp,
+            sourceUrl: Willis-MD,
+            mediaType: 1,
+            renderLargerThumbnail: false
+          }
+        }
+    };
+    
+    await conn.sendMessage(m.chat, doc, { quoted: m });
+}
+handler.help = ['spotify'];
+handler.tags = ['downloader'];
+handler.command = /^(spotify|song)$/i;
+
+export default handler;
